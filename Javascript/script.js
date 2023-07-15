1a71b0a9b1e8954c6f6edda6a4242c4e478ff182
urvashi2410 <urvashianand0906@gmail.com>
2023-07-15 15:38:38 +0530

    all the classes of batch-2 till now

---
+++ Javascript/script.js
@@ -0,0 +1,57 @@
+// document.querySelector(".heading").textContent = "Hello world!";
+
+// document.querySelector("#heading1").textContent = "Age: 34";
+
+// console.log("Baodan Nguyen");
+
+// literals : you cannot change its value..
+
+// 34 
+// 56
+// 45.8
+// 'Pthan'
+
+// variables : you can change its value..
+
+// let age = 34;
+// console.log(age);
+
+// naming conventions to name a variable: 
+
+// let uLAURA = 34;
+// console.log(uLAURA);
+
+// let Ulaura = 34;
+// console.log(Ulaura);
+
+// camel case
+
+// lowercase everywhere , we can start with a lowercase 
+// let lauranguyen = 25;
+// console.log(lauranguyen);
+
+// uppercase everywhere , we can start with a uppercase
+// let LAURANGUYEN = 25;
+// console.log(LAURANGUYEN);
+
+// number at the start is not allowed 
+// let 9Pthan = 45;
+// console.log(9pthan);
+
+// number at the end works 
+// let pthan34 = 45;
+// console.log(pthan34);
+
+// number in the middle works 
+// let pth23an = 56;
+// console.log(pth23an);
+
+// underscores work everywhere 
+// let _richard = 1;
+// console.log(_richard);
+
+// let richa_rd = 0;
+// console.log(richa_rd);
+
+// let richard_ = 89;
+// console.log(richard_);
