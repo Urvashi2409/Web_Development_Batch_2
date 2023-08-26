b640450ae5b2ef44d1116ba155a7e1cda8e05fd2
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-26 23:46:35 +0530

    code

---
+++ Flask/test.py
@@ -0,0 +1,11 @@
+mylist = [1, 2, 3, 4, 6, 10, 3, 4, 5, 6, 7]
+
+# print(mylist[2:6])
+# print(mylist[-2:6])
+# print(mylist[:])
+# print(mylist[-1:])
+
+# print(mylist[-2:1:-2]) = baodan  
+# print(mylist[-2:-7:-2]) celina 
+# print(mylist[-2:8:-1]) = angelina 
+# print(mylist[8:-1:-2]) = ethan
--- Javascript/script.js
+++ Javascript/script.js
@@ -216,27 +216,97 @@ let age = 34;
 //     console.log("dont know");
 // }
 
-if(2 == 3){
-    console.log("equal");
-}
-if(2 == 4){
-    console.log("not equal");
-}
-if(2 == 5){
-    console.log("dont know");
-}
-if(false == true){
-    console.log("not possible");
-}
-if(true){
-    console.log("true");
-}
-else if(5 != 3){
-    console.log("not equal 5 and 3");
-}
-else if(4 != 4){
-    console.log("not equal 4 and 4");
-}
-else{
-    console.log("Default value!!");
-}
+// if(2 == 3){
+//     console.log("equal");
+// }
+// if(2 == 4){
+//     console.log("not equal");
+// }
+// if(2 == 5){
+//     console.log("dont know");
+// }
+// if(false == true){
+//     console.log("not possible");
+// }
+// if(true){
+//     console.log("true");
+// }
+// else if(5 != 3){
+//     console.log("not equal 5 and 3");
+// }
+// else if(4 != 4){
+//     console.log("not equal 4 and 4");
+// }
+// else{
+//     console.log("Default value!!");
+// }
+
+// x = 12;
+// y = 3;
+// z = 11;
+// if(x == 2 && x < 3){
+//     console.log(x);
+// }
+// if(x != 5){
+//     console.log("whatever");
+// }
+// if(x != 5 && y >= 5){
+//     console.log("its gonna happen");
+// }
+// if(z != 0 || x == 2){
+//     console.log("you say");
+// }
+// if(!(y < 10)){
+//     console.log(y);
+// }
+// else if(x < 10 || x < 5){
+//     console.log("okkkkkkk");
+// }
+// else if(y < 10 || y <= 0){
+//     console.log("pikachu");
+// }
+// else if(z == 0 && y ==5){
+//     console.log("Pikaboo");
+// }
+// else{
+//     console.log("Default value");
+// }
+
+
+// x = 2;
+// y = 30;
+// z = 12;
+// if(x >= 4 && x <= 2){
+//     console.log(x);
+// }
+// if(x  != -15){
+//     console.log("whatever");
+// }
+// if(x == 5 && y <= 4){
+//     console.log("its gonna happen");
+// }
+// if(z > x && x < y || x == 2){
+//     console.log("you say");
+// }
+// if(!(y < 3)){
+//     console.log(y);
+// }
+// else if(y < 10 || z < 5){
+//     console.log("okkkkkkk");
+// }
+// else if(z > 10 || y <= 100){
+//     console.log("pikachu");
+// }
+// else if(z == 0 && y == 5){
+//     console.log("Pikaboo");
+// }
+// else{
+//     console.log("Default value");
+// }
+
+// DOM Manipulation 
+// how to change the css styles using javascript : 
+
+document.querySelector(".body").style.backgroundColor = "rgba(255,255,5)";
+document.querySelector(".heading").style.color = "rgba(4, 255, 4)";
+document.querySelector(".heading").style.fontSize = "100px";
