446a154d4002d4d2ee36ebfe883b9e8191315f47
Urvashi2409 <urvashicode2410@gmail.com>
2023-08-28 00:51:06 +0530

    flask app is running with index.html in it:

--- Flask/basic.py
+++ Flask/basic.py
@@ -33,9 +33,10 @@ from flask import render_template, Flask
 
 app = Flask(__name__)
 
-@app.route('')
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
+
